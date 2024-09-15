4f959dc2c26181de25ad80904668997f34bfa5ec
ovkrishna <ovkrishna048@gmail.com>
2024-09-15 08:03:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-AZURE_API_KEY = "fce9e5831f0342988998803cd142a2aa"  # Replace with your Azure OpenAI API key
-AZURE_ENDPOINT = "https://ragsac1.openai.azure.com/"  # Replace with your Azure OpenAI endpoint URL
+AZURE_API_KEY = "type your ap_key here"  # Replace with your Azure OpenAI API key
+AZURE_ENDPOINT = "type your endpint here"  # Replace with your Azure OpenAI endpoint URL
 PDF_PATH = "data source/sample_pdf.pdf"  # Path to the PDF document
 CHROMADB_COLLECTION = "pdf_embeddings"       # Name of the ChromaDB collection
